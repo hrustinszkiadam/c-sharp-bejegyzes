252269851054fe30eeb2af83090b4923e84b288b
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-01-10 12:40:11 +0100

    2.b

--- Program.cs
+++ Program.cs
@@ -14,6 +14,26 @@ internal class Program
             new Bejegyzes("Első bejegyzés", "Ez az első bejegyzés szövege"),
             new Bejegyzes("Második bejegyzés", "Ez a második bejegyzés szövege")
         ];
+
+        int ujDarab = 0;
+        Console.Write("Hány új bejegyzést szeretnél létrehozni? ");
+        ujDarab = int.Parse(Console.ReadLine() ?? "1");
+        for (int i = 0; i < ujDarab; i++)
+        {
+            string szerzo = "";
+            string tartalom = "";
+            while (szerzo == "")
+            {
+                Console.Write("Szerző: ");
+                szerzo = Console.ReadLine() ?? "";
+            }
+            while (tartalom == "")
+            {
+                Console.Write("Tartalom: ");
+                tartalom = Console.ReadLine() ?? "";
+            }
+            bejegyzesek.Add(new Bejegyzes(szerzo, tartalom));
+        }
         Console.ReadKey();
     }
 }
